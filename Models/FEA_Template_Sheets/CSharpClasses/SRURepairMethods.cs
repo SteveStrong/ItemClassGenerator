3a9acdc2d8ecb069feaf16d68dcfc388091505ea
Justin Choi <choiju@ado.readyone.net>
2024-10-29 13:11:34 -0700

    readd annotations

--- Models/FEA_Template_Sheets/CSharpClasses/SRURepairMethods.cs
+++ Models/FEA_Template_Sheets/CSharpClasses/SRURepairMethods.cs
@@ -1,14 +1,22 @@
-using System;
-using System.Collections.Generic;
+using ExternalLibrary;
 
 namespace GeneratedClasses
 {
     public class SRURepairMethods
     {
+        [MapFrom(typeof(ExternalClass1), "arasProp1")]
         public string? SruId { get; set; }
+
+        [MapFrom(typeof(ExternalClass1), "arasProp2")]
         public string? SruName { get; set; }
+
+        [MapFrom(typeof(ExternalClass2), "arasProp1")]
         public string? ResourceId { get; set; }
+
+        [MapFrom(typeof(ExternalClass2), "arasProp2")]
         public string? ResourceName { get; set; }
+
+        [MapFrom(typeof(ExternalClass2), "arasProp3")]
         public string? ResourceUtilizationTime { get; set; }
     }
 }
